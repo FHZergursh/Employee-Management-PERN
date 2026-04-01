5cc32b78a8baf2ca92e5067e0cb74fa9df45316a
FHZergursh <89704618+FHZergursh@users.noreply.github.com>
2026-04-01 19:51:49 +0100

    init
    title

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import { Header } from "./components/Header"
+
+const App = () => {
+  return (
+    <div >
+      <Header />
+
+      <div className="min-h-auto flex justify-center mt-[5vh]">Table would go here</div>
+
+      
+
+
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,12 @@
+
+
+export const Header = () => {
+  return (
+    <div className='min-h-[10vh]'>
+      <div className="flex justify-between pt-7 mx-[20vw]" >
+        <div className="text-2xl">Employee Management</div>
+        <button className="text-1xl bg-slate-200 px-2 rounded-md ">Add Employee</button>
+      </div>
+    </div>
+  )
+}
