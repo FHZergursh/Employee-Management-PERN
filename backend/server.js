2ebce86e67a02e950ebf42d1221656f679417324
FHZergursh <89704618+FHZergursh@users.noreply.github.com>
2026-04-08 02:07:31 +0100

    link frontend to backend
    title, using a simple react fetch to do this, though axios is in the back pocket if I ever end up needing it

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from "express"
+import cors from "cors"
 import employeeRoutes from "./routes/employeeRoute.js"
 import { setupDB } from "./db/setupDB.js";
 
@@ -6,6 +7,7 @@ const app = express();
 const PORT = 3000
 
 app.use(express.json())
+app.use(cors());
 
 app.use("/api/employees", employeeRoutes)
 
